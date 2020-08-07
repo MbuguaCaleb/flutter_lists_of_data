55d65a8a1e7015c92e070926ca2506726e322d0d
MbuguaCaleb <mbuguacaleb30@gmail.com>
2020-08-07 13:46:17 +0300

    'modularizing-code-into-having-reusable-classes'

---
+++ lib/quote_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+
+  /*variable instantiation into the class as a quote object*/
+  /* I should ask myself am i instantiating a String,Int,object or a widget
+  This is what the prefix upfront means
+   */
+  final Quote quote;
+
+  QuoteCard({this.quote});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0,16.0,16.0,0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey[600]
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[800]
+              ),
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+}
